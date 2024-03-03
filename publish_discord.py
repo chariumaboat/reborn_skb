71af747efe4d33695a7a42ba463334d82c72b51b
unknown <60532865+chariumaboat@users.noreply.github.com>
2024-03-03 18:11:08 +0900

    ランダム投稿機能の作成

--- publish_discord.py
+++ publish_discord.py
@@ -4,8 +4,11 @@ import json
 from pprint import pprint
 import hashlib
 import sys
+import os
+from discordwebhook import Discord
 
 NO_IMG_HASH = '5d6737777450ade1eae0106458ddaea4'
+DISCORD_WEBHOOK_URL = os.environ['DISCORD_WEBHOOK_URL']
 
 
 def get_random_media_key():
@@ -25,8 +28,17 @@ def get_md5(path):
     return hashlib.md5(data).hexdigest()
 
 
+def port_discord(path, text):
+    discord = Discord(url=DISCORD_WEBHOOK_URL)
+    with open(path, 'rb') as f:
+        discord.post(content=text, file={"attachment": f})
+
+
 # 取得したリストが空ではない
 # 画像のmd5が5d6737777450ade1eae0106458ddaea4ではない場合ループを抜ける
+
+# TODO
+# リファクタリングする、jsonから投稿文も作成する
 while True:
     media_key = get_random_media_key()
     if media_key:
@@ -34,4 +46,6 @@ while True:
             h = get_md5(i)
             if h != NO_IMG_HASH:
                 print(media_key)
+                for i in media_key:
+                    port_discord(i, 'test')
                 sys.exit()
